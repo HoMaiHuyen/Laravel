2c87b10ac7559673d725bcd4b0c80db3fcd8b9fe
HoMaiHuyen <huyen.homai25@student.passerellesnumeriques.org>
2024-02-01 00:52:01 +0700

    Routes in Laravel

--- hocLaravel/routes/web.php
+++ hocLaravel/routes/web.php
@@ -1,7 +1,7 @@
 <?php
-use Illuminate\Support\Facades\Route;
-use App\Models\Use;
 
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,7 +13,69 @@ use App\Models\Use;
 |
 */
 
-Route::get('/', function(){
-    $html = '<h1>Hoc Lap Trinh</h1>';
-    return $html;
+// Route::get('/', function () {
+//     $html = '<h1>Hoc Lap Trinh</h1>';
+//     return $html;
+// });
+// Route::get('unicode', function () {
+//     return view('form');
+// });
+// Route::get('unicode', function () {
+//     return 'Phuong thuc get cua path /unicode';
+// });
+
+// Route::post('/unicode', function () {
+//     return 'Phuong thuc Post cua path /unicode';
+// });
+
+// Route::put('unicode', function(){
+//     return "Phuong thuc Put cua path /unicode";
+// });
+
+// Route::delete('unicode', function () {
+//     return "Phuong thuc Delete cua path /unicode";
+// });
+
+// Route::patch('unicode', function () {
+//     return "Phuong thuc Patch cua path /unicode";
+// });
+
+// Route::match(['get', 'post'], 'unicode', function(){
+//     return $_SERVER['REQUEST_METHOD'];
+// });
+
+// Route::any('unicode', function(Request $request){
+//     return $request->method();
+// });
+// Route::get('show_form', function(){
+//     return view('form');
+// });
+
+// Route::redirect('unicode', 'show_form',404);
+
+// Route::view('show_form', 'form');
+Route::prefix('admin')->group(function(){
+    Route::get('unicode', function () {
+        return 'Phuong thuc get cua path /unicode';
+    });
+
+    Route::get('show_form', function () {
+        return view('form');
+    });
+    Route::prefix('products')->group(function(){
+        Route::get('/', function(){
+            return 'Danh sach san pham';
+        });
+
+        Route::get('add', function(){
+            return 'them san pham';
+        });
+        Route::get('edit', function () {
+            return 'sua san pham';
+        });
+        Route::get('delete', function () {
+            return 'xoa san pham';
+        });
+    });
 });
+ 
