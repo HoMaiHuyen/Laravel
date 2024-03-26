92e213f97b10947adea0a13f6c3ef90f78e31767
HoMaiHuyen <143241055+HoMaiHuyen@users.noreply.github.com>
2024-03-26 22:23:28 +0700

    Query Builder Database (#35)

--- hocLaravel/app/Models/Users.php
+++ hocLaravel/app/Models/Users.php
@@ -41,47 +41,50 @@ class Users extends Model
     }
 
     public function learQueryBuilder(){
-        DB::enableQueryLog();
-        //Lay tat ca ban ghi cua table
-        // $id = 20;
-        // $lists = DB::table($this->table)
-        // ->select('fullname as hoten', 'email', 'id', 'updated_at', 'created_at')
-        // ->whereDate('updated_at', '2022-01-21 20:00:00')
-        // ->whereMonth('created_at', '3')
-        // ->whereMonth('created_at', '18')
-        // ->whereYear('created_at', '2024')
-        // ->whereColumn('created_at', '=', 'updated_at')
-        // ->get();
-        // ->toSql();
-        // dd($lists);
 
-        // $list = DB:: table('users')
-        // ->select('users.*', 'groupps.name as group_name')
-        // ->leftJoin('groupps', 'users.group_id', '=', 'groupps.id')
-        // ->get();
-
-        $list = DB::table('users')
-            // ->select('users.*', 'groupps.name as group_name')
-            // ->rightJoin('groupps', 'users.group_id', '=', 'groupps.id')
-            // ->orderBy('id', 'desc')
-            // ->orderBy('created_at', 'desc')
-            // ->orderBy('created_at', 'asc')
-            // ->orderBy('id', 'desc')
-            // ->inRandomOrder()
-
-            // ->select(DB::raw('count(id) as email_count'), 'email', 'fullname')
-            // ->groupBy('email')
-            // ->groupBy('fullname')
-            // ->having('email_count', '>=', 2)
-            // ->limit(2)
-            // ->offset(0)
-            ->take(2)
-            ->skip(2)
-            ->get();
 
         // return ($list);
 
-        dd($list);
+        // $status = DB::table('users')->insert([
+        //     'fullname' => 'Hồ Mai Huyền',
+        //     'email' => 'huyen@gmail.com',
+        //     'group_id' => 1,
+        //     'created_at' => date('Y-m-d H:i:s')
+        // ]);
+
+        // dd($status);
+
+        // $lastId = DB::getPdo()->lastInsertId();
+
+        // $lastId = DB::table('users')->insertGetId([
+        //     'fullname' => 'Hồ Mai Huyền',
+        //     'email' => 'huyen@gmail.com',
+        //     'group_id' => 1,
+        //     'created_at' => date('Y-m-d H:i:s')
+        // ]);
+
+        // dd($lastId);
+
+        // $status = DB::table('users')
+        // ->where('id', 29)
+        // ->update([
+        //     'fullname' => 'Hồ Mai Huyền',
+        //     'email' => 'huyen@gmail.com',
+        //     'updated_at' => date('Y-m-d H:i:s')
+        // ]);
+
+        // dd($status);
+
+        // $status = DB::table('users')
+        // ->where('id', 9)
+        // ->delete();
+
+        //Đếm số bản ghi
+        $list = DB::table('users')->where('id', '>', 20)->count();
+
+        $list = DB::table('users')->where('id', '>', 20)->get();
+        $count = count($list);
+        dd($count);
         $sql = DB::getQueryLog();
         dd($sql);
         //Lay ban ghi dau tien cua table (Lay thong tin chi tiet)
