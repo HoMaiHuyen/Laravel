3a1e8e23bfeb1d289a3db1e91310aac61a3f1bc5
HoMaiHuyen <huyen.homai25@student.passerellesnumeriques.org>
2024-03-28 01:48:07 +0700

    Query Builder Database

--- hocLaravel/app/Models/Users.php
+++ hocLaravel/app/Models/Users.php
@@ -80,14 +80,44 @@ class Users extends Model
         // ->delete();
 
         //Đếm số bản ghi
-        $list = DB::table('users')->where('id', '>', 20)->count();
+        // $list = DB::table('users')->where('id', '>', 20)->count();
 
-        $list = DB::table('users')->where('id', '>', 20)->get();
-        $count = count($list);
-        dd($count);
+        // $list = DB::table('users')->where('id', '>', 20)->get();
+        // $count = count($list);
+        // dd($count);
+
+        DB:: table('users')
+        // ->select(
+        //     // DB::raw('count(id) as email_count')
+        //     // DB::raw("'fullname' as hoten 'email'")
+            
+        // )
+        // ->selectRaw('email, fullname, count(id)')
+        // ->groupBy('email')
+        // ->groupBy('fullname')
+        // ->where(DB::raw('id>20'))
+        // ->where('id > ?', [20])
+        // ->whereRaw('id>20')
+        // ->orwhereRaw('id>20')
+        // ->orderByRaw('created_at DESC, updated_at ASC')
+        // ->groupByRaw('email, fullname')
+        // ->havingRaw('email_count', '>=', 2)
+        // ->where('group_id', '=', DB::raw(
+        //     'group_id',
+        //     '=',
+        //     function($query){
+        //         $query->select('id')
+        //         ->from('groupps')
+        //         ->where('name', '=', 'Admin');
+        //     })
+        //     )
+        ->select('email', DB::raw('(SELECT count(id) FROM "groupps") as group_cpunt'))
+        ->get();
         $sql = DB::getQueryLog();
         dd($sql);
         //Lay ban ghi dau tien cua table (Lay thong tin chi tiet)
         $detail = DB::table($this->table)->first();
     }
+
+
 }
